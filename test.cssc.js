1897b4580639b27a6e541831176f6e89b7433287
melPavel <pavel@wtnet.de>
2016-08-16 17:28:21 +0200

    Update test.cssc.js

--- test.cssc.js
+++ test.cssc.js
@@ -1,24 +1,51 @@
+var randomColor = function()
+{
+    return "rgb("+parseInt(Math.random()*255)+","+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
+};
+
 CSSC({
-    "body": {
-        background-color: "gray",
-        width: "10px",
-        height: function()
+    "body": 
+    {
+        'background-color': randomColor(),
+        'width': function()
         {
-            return window.innerHeight+"px";
-        }
+            return (window.innerWidth)+"px";
+        },
+        'height': function()
+        {
+            return (window.innerHeight)+"px";
+        },
+        "overflow": "hidden",
+        'margin': 0
     },
-    ".test": {
-        color: function()
+    ".test": 
+    {
+        'color': randomColor, //updatable
+        'background': randomColor(), //not updatable
+        'font-size': function(){return parseInt(Math.random() * (25 - 15) + 15)+"px"},
+        'position': "relative",
+        'float': "left",
+        'margin': function()
         {
-            return "rgb("+parseInt(Math.random()*255)+","+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
+            return parseInt(window.innerHeight * 0.05 / 2)+ "px "+parseInt(window.innerWidth * 0.05 / 2) + "px";
+        },
+        'width': function()
+        {
+            return parseInt(window.innerWidth * 0.2)+"px";
+        },
+        'height': function()
+        {
+            return parseInt(window.innerHeight * 0.45)+"px";
         }
-        font-size: parseInt(Math.random()*25)+"px",
-        position: "absolute"
     },
     "@media screen and (max-width: 680px)": 
     {
         ".test":{
-            font-size: parseInt(Math.random()*15)+"px"
+            'font-size': parseInt(Math.random()*15)+"px"
         }
     },
+});
+window.addEventListener("resize", function()
+{
+    CSSC.update();
 });
